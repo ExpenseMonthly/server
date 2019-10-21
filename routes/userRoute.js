df7f9bc761be55f36f1b17657c28089b21b3367b
das <ardynn.d@gmail.com>
2019-10-21 15:39:27 +0700

    add get user info

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const UserController = require('../controllers/userController');
+const Authentication = require('../middleware/authentication');
 const { multer, getPublicUrl, sendUploadToGCS } = require('../middleware/image');
 
 const router = express.Router();
 
 router.post('/register', multer.single('profile_url'), sendUploadToGCS, UserController.register);
 router.post('/login', UserController.login);
+
+
+router.get('/', Authentication, UserController.getUser);
+router.get('/info', Authentication, UserController.getProfile);
 
 module.exports = router;
