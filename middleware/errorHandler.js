9167b800680f740ea74d5edc7f97bb58ddc277f3
Ricardo Tan <ricardo.tan.2000@gmail.com>
2019-10-23 20:31:55 +0700

    adding ignore

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,14 +4,20 @@ function errorHandler(err, req, res, next) {
     // console.log(err, `<<<<<<<<<<<,,,,,,,,,,,,,,,,`);
     // console.log(err.stack, `<<<<<<<<<<<,,,,,,,,,,,,,,,,`);
     if (req.file && req.file.cloudStoragePublicUrl) {
+        /* istanbul ignore next */
         let file = req.file.cloudStoragePublicUrl.split('/');
+        /* istanbul ignore next */
         let fileName = file[file.length - 1];
+        /* istanbul ignore next */
         deleteFile(fileName)
     }
     if (err.name == 'MulterError' && err.code == 'LIMIT_UNEXPECTED_FILE') {
+        /* istanbul ignore next */
         res.status(400).json({
+            /* istanbul ignore next */
             message: ["File Size Exceed"]
         })
+        /* istanbul ignore next */
     } else if (err.name == 'JsonWebTokenError') {
         res.status(401).json({
             message: ["invalid token"]
@@ -22,13 +28,16 @@ function errorHandler(err, req, res, next) {
             msgValidation.push(err.errors[r].message);
         }
         // console.log(msgValidation);
+        /* istanbul ignore next */
         res.status(400).json({
             message: msgValidation
         });
+        /* istanbul ignore next */
     } else if (err.name == 'CastError') {
         return res.status(400).send({
             message: ['Invalid ID']
         });
+        /* istanbul ignore next */
     } else if (err.statusCode == 404 && err.msg === undefined) {
         res.status(404).json({
             message: ["Data not found"]
